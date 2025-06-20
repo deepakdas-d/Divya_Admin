de9521dd63b07fdd996c74d3c42a29a62e3622bd
Deepa <deepakdev9567@gmail.com>
2025-06-20 12:10:29 +0530

    add new page

---
+++ admin/lib/Salesperson_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SalespersonList extends StatefulWidget {
+  const SalespersonList({super.key});
+
+  @override
+  State<SalespersonList> createState() => _SalespersonListState();
+}
+
+class _SalespersonListState extends State<SalespersonList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
